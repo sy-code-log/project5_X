658f8e94179e073b43c5c0e217b8c109652dcbc4
소영 <soyoung84349@gmail.com>
2025-09-28 21:34:31 +0900

    Update new_add.py

--- new_add.py
+++ new_add.py
@@ -425,7 +425,7 @@ st.markdown(
     <style>
         /* 전체 페이지 상단 여백 제거 */
         .block-container {
-            padding-top: 2rem;
+            padding-top: 3rem;
         }
     </style>
     """,
